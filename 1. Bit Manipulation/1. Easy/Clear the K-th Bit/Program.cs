b916f5f6367430b5aaa5ba4049a39d9517c3a2da
Karan Dobriyal <karandobriyal26@gmail.com>
2023-02-26 13:36:31 +0530

    solution

--- 1. Bit Manipulation/1. Easy/Clear the K-th Bit/Program.cs
+++ 1. Bit Manipulation/1. Easy/Clear the K-th Bit/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution {
+    public static void Main(String[] args) {
+
+        int n = 11;
+        int k = 4;
+        Console.WriteLine(ClearSetBit(n,k));
+    }
+    static int ClearSetBit(int n, int k)
+    {
+        n = n & (~(1<<k-1));
+        return n;
+    }
+}
