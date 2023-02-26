cd2f6543d5216173c7dbbc4dcb7e5a3259f3e617
Karan Dobriyal <karandobriyal26@gmail.com>
2023-02-26 13:45:27 +0530

    solution

---
+++ 1. Bit Manipulation/1. Easy/Binary Number with Alternating Bits/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public static void Main(String[] args)
+    {
+         int n = 7;
+         Console.WriteLine(hasAlternateBits(n));
+    }
+        static bool hasAlternateBits(int n)
+        {
+            bool res = true;
+            while(n!=0)
+            {
+                int a = (n&1);
+                int b = ((n>>1) & 1);
+                if(a == b)
+                {
+                    res = false;
+                    break;
+                }
+               n>>=1;
+            }
+            return res;
+        }
+}
