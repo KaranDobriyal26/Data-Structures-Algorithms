a7e1e484c1e8ac6a5845219a1e0a8d13a8242763
Karan Dobriyal <karandobriyal26@gmail.com>
2023-02-26 13:34:00 +0530

    solution

--- 1. Bit Manipulation/1. Easy/Checking Whether K-th bit is Set or Not/Program.cs
+++ 1. Bit Manipulation/1. Easy/Checking Whether K-th bit is Set or Not/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution {
+    public static void Main(String[] args) {
+        int n = 5; // 101;
+        int k = 1; // 001;
+       Console.WriteLine(isSetBit(n,k));
+    }
+
+    static bool isSetBit(int n, int k)
+    {
+         // 1. Approach Using Masking
+        return ((n & (1<<k)) >0);
+
+        // 2. Approach is Using right Shift.
+        //return (((n>>k)&1)==1);
+    }
+}
