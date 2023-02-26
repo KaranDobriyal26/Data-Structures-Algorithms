40cc0e2259847eb36afbeef1da9542b8588e0daf
Karan Dobriyal <karandobriyal26@gmail.com>
2023-02-26 13:40:48 +0530

    solution

---
+++ 1. Bit Manipulation/1. Easy/Set the K-th bit/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public static void Main(String[] args) {
+        // Set the Kth Set bit.
+        int n = 5;
+        int k = 1;
+        Console.WriteLine(SetTheKthBit(n,k));
+    }
+    static int SetTheKthBit(int n, int k)
+    {
+        int mask = 1 << k;
+        n |= mask;
+        return n;
+    }
+}
